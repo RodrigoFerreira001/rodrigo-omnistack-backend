f1f5458baa197b650f5740a029dd385065eb10fa
Rodrigo Ferreira <rodrigoferreira001@hotmail.com>
2019-06-04 23:30:21 -0300

    correção do módulo File.js

--- src/models/File.js
+++ src/models/File.js
@@ -18,7 +18,7 @@ const File = new mongoose.Schema({
 
 File.virtual("url").get(function(){
     const url = process.env.URL || 'http://localhost:8889';
-    return `${url}/files/${encodeURIComponent(this.path)};
+    return `${url}/files/${encodeURIComponent(this.path)}`;
 });
 
 module.exports = mongoose.model("File", File)
